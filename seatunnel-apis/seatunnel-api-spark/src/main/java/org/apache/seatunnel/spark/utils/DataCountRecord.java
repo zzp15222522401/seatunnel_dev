2e04a6f955e02fdb18226b65d82d5740790acf26
zpzhang <zpzhang@aibee.com>
2022-06-28 16:27:51 +0800

    更新数据量记录方式

--- seatunnel-apis/seatunnel-api-spark/src/main/java/org/apache/seatunnel/spark/utils/DataCountRecord.java
+++ seatunnel-apis/seatunnel-api-spark/src/main/java/org/apache/seatunnel/spark/utils/DataCountRecord.java
@@ -14,23 +14,47 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DataCountRecord {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DataCountRecord.class);
 
-    public static long sourceDataCount(Dataset<Row> source) {
+    private static long sourceDataCount(Dataset<Row> source) {
         return source.count();
     }
 
-    public static long transformDataCount(Dataset<Row> tranform) {
+    private static long transformDataCount(Dataset<Row> tranform) {
         return tranform.count();
     }
 
     public static long sinkDataCount(Dataset<Row> sink) {
         return sink.count();
+    }
+
+    private static String getPropEnv(Config config) {
+        String propEnv;
+        if (config.hasPath("spark.yarn.appMasterEnv.prop_env")) {
+            propEnv = config.getString("spark.yarn.appMasterEnv.prop_env");
+        }
+        else {
+            propEnv = "dev";
+        }
+        return propEnv;
+    }
+
+    private static String getIfRecord(Config config) {
+        String isRecord;
+        if (config.hasPath("spark.yarn.appMasterEnv.if_record")) {
+            isRecord = config.getString("spark.yarn.appMasterEnv.if_record");
+        }
+        else {
+            isRecord = "false";
+        }
+        return isRecord;
     }
 
     public static void dataCount(SparkEnvironment environment, List<SparkBatchSource> sources, List<BaseSparkTransform> transforms, List<SparkBatchSink> sinks) throws SQLException, IOException, ClassNotFoundException {
@@ -42,20 +66,8 @@ public class DataCountRecord {
 
         Config config = environment.getConfig();
         String appName = config.getString("spark.app.name");
-        String propEnv;
-        String isRecord;
-        if (config.hasPath("spark.yarn.appMasterEnv.prop_env")) {
-            propEnv = config.getString("spark.yarn.appMasterEnv.prop_env");
-        }
-        else {
-            propEnv = "dev";
-        }
-        if (config.hasPath("spark.yarn.appMasterEnv.if_record")) {
-            isRecord = config.getString("spark.yarn.appMasterEnv.if_record");
-        }
-        else {
-            isRecord = "false";
-        }
+        String isRecord = DataCountRecord.getIfRecord(config);
+        String propEnv = DataCountRecord.getPropEnv(config);
         if (!"false".equals(isRecord)) {
             String tableName = "Source";
             if (!sources.isEmpty()) {
@@ -103,22 +115,20 @@ public class DataCountRecord {
             LOGGER.info("resultStr:" + resultCount);
             String appId = GetYarnInFor.getYarnInForDetail(propEnv, appName);
             LOGGER.info("appId:" + appId);
-            String sqlStr = String.format("update \n" +
-                    "dis_job_detailed_information \n" +
-                    "set \n" +
-                    "data_count='%s', application_id='%s' \n" +
-                    "where \n" +
-                    "job_name = '%s' \n" +
-                    "and job_submit_time in (\n" +
-                    "select \n" +
-                    "a.atime \n" +
-                    "from \n" +
-                    "(select \n" +
-                    "max(job_submit_time) as atime \n" +
-                    "from dis_job_detailed_information \n" +
-                    "where \n" +
-                    "job_name = '%s') as a)", resultCount, appId, appName, appName);
-            GetConnectMysql.saveToMysql(sqlStr, propEnv);
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String now = dateFormat.format(date);
+            String insertSql = String.format("insert into dis_job_detailed_information (job_name,fieldmapping,job_submit_time,dag_id,pre_task_id,task_id,\n" +
+                                             "create_time,sqlfilename,propfilename, source_name, target_name,\n" +
+                                             "application_id,data_count,job_status,final_status)" +
+                                             "select a.job_name,fieldmapping,'%s',dag_id,pre_task_id,task_id,'%s', \n" +
+                                             "sqlfilename, propfilename, a.source_name, a.target_name, '%s','%s','%s','%s' \n" +
+                                             "from dis_job_detailed_information a left join dis_instance_record b on a.job_name = b.job_name \n" +
+                                             "and a.source_name =b.source_name and a.target_name =b.target_name where a.job_name = '%s' and b.job_name ='%s'\n" +
+                                             "order by a.create_time desc limit 1",
+                                             now, now, appId, resultCount, "FINISHED", "SUCCEEDED", appName,  appName);
+            LOGGER.info(insertSql);
+            GetConnectMysql.saveToMysql(insertSql, propEnv);
             LOGGER.info("Data volume is recorded");
         }
         else {
